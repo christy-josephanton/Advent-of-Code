0969fba2666f77cad4e077b09990b05d91ee35a7
Christy Joseph-Anton <christy.josephanton@gmail.com>
2023-12-04 10:00:22 -0500

    feat: day 3 part 1

--- 3/part1.py
+++ 3/part1.py
@@ -1,61 +1,38 @@
 sum=0
 file = open('input.txt','r')
-lineP='.'*141
-lineC='.'*140
-
+lineP='.'*140+'\n'
+lineC='.'*140+'\n'
 
 def isSymbol(s):
+    return not s.isnumeric() and s!='.' and s!='\n'
 
-    if not s.isnumeric() and s!='.' and s!='\n':
-        return True
+lines = file.readlines()
+lines[len(lines)-1]+='\n'
+lines.append('.'*140+'\n')
 
+for x in range(len(lines)):
+    lineN=lines[x]
 
-    return False
-    
-
-for lineN in file:
-  
-    lineC=lineN
-    lineC='.1.61........397...#386...=........313........-...&............*......*................@.............../.........621+....................169\n'
-    lineN='.../..........*574.587..*........1/1......904.......412.........*.................*.................................=.....637.%......*..../.\n'
-
-    print(lineP)
-    print(lineC)
-    print(lineN)
-
-    #print(lineN)
     numFound=False
     buildNum=''
     valid=False
     prevValid=False
+    nextValid=False
 
     for i in range(len(lineC)):
-    
-        lineP[i]
-        lineC[i]
-        lineN[i]
 
-        #TODO add checking for adj in next columns
-
+        if(lineC[i]!='\n' ):
+            nextValid=isSymbol(lineP[i+1]) or isSymbol(lineC[i+1]) or isSymbol(lineN[i+1])
 
         if(not lineC[i].isnumeric() and numFound):
-            
-    
-            print('Checking Number: '+str(buildNum))
-            #check if valid
-            if(valid):
-                print('Valid!')
-                sum+=int(buildNum)
+            if(valid): sum+=int(buildNum)
             valid=False
-
-            
             numFound=False
             buildNum=''
 
-
         if(lineC[i].isnumeric()):
             
-            if(isSymbol(lineP[i]) or isSymbol(lineN[i]) or isSymbol(lineP[i]) or isSymbol(lineN[i]) or prevValid):
+            if(isSymbol(lineP[i])or isSymbol(lineN[i]) or prevValid or nextValid):
                 valid=True
 
             numFound=True
@@ -63,10 +40,7 @@ for lineN in file:
         
         #check if symbol is adj in prev
         prevValid=isSymbol(lineP[i]) or isSymbol(lineC[i]) or isSymbol(lineN[i])
-            
-
-    break
-
-    #move on to next line
     lineP=lineC
     lineC=lineN
+
+print(sum)
